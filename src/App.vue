a38e5e1c39c42f98247dc34b001ddba031718320
Rob McCormack <mrmccormack@gmail.com>
2018-09-20 19:05:14 -0400

    added MrM
    added Mr. M.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App from App.vue" msg2="BOB msg2" />
   </div>
 </template>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h4>Here it is: {{ msg2 }}</h4>
     <p>
       For guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -35,7 +36,8 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: String,
+    msg2: String
   }
 }
 </script>
